ec1f5ad7765c4264f2fe31adb39ae935ddba59e5
Iburinoc <iburinoc@gmail.com>
2014-01-21 18:13:01 -0500

    trick tricky

--- src/client/controllers/Cntrls.js
+++ src/client/controllers/Cntrls.js
@@ -1,13 +1,3 @@
 function MainCtrl($scope) {
-	$scope.username = '';
-	$scope.validateUsername = function () {
-		console.log($scope.username);	
-	}
-    $scope.joinMouseOver = false;
-    
-    $scope.validateID = function () {
-        
-    }
-    $scope.gameID = '';
     
 }
