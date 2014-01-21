2cdb06b8b26994fd494a56652e6b07c1c7e5174c
Iburinoc <iburinoc@gmail.com>
2014-01-21 16:19:08 -0500

    Ok almost done main page.  This is gonna require a lot of backend support but lets get the front end done first.

---
+++ src/client/Cntrls.js
@@ -0,0 +1,13 @@
+function MainCtrl($scope) {
+	$scope.username = '';
+	$scope.validateUsername = function () {
+		console.log($scope.username);	
+	}
+    $scope.joinMouseOver = false;
+    
+    $scope.validateID = function () {
+        
+    }
+    $scope.gameID = '';
+    
+}
--- src/client/server.js
+++
@@ -1,2 +0,0 @@
-var mafia = angular.module('mafia', []);
-var ip = '99.225.251.49';
